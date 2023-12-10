bbe26dfc426efd62c6a822aa1e79af1f5e757872
Max Verbeek <m4xv3rb33k@gmail.com>
2023-12-10 18:13:59 +0100

    feat: idk

--- 10/main.swift
+++ 10/main.swift
@@ -34,6 +34,26 @@ let east: Direction = (dy: 0, dx: 1)
 let south: Direction = (dy: 1, dx: 0)
 let west: Direction = (dy: 0, dx: -1)
 
+func letterToDir(_ letter: Character) -> Direction? {
+    switch letter {
+    case "N": return north
+    case "E": return east
+    case "S": return south
+    case "W": return west
+    case _: print("letter does not have direction: ", letter); assert(false)
+    }
+}
+
+func dirToLetter(_ dir: Direction) -> Character {
+    switch dir {
+    case north: return "N"
+    case east: return "E"
+    case south: return "S"
+    case west: return "W"
+    case _: print("direction does not have letter", dir); assert(false)
+    }
+}
+
 func findStart(_ grid: [String]) -> Location? {
     for (rowindex, row) in grid.enumerated() {
         if let col = row.firstIndex(of: "S") {
@@ -52,6 +72,10 @@ func lookup(map: [String], y: Int, x: Int) -> Character? {
     let row = map[y]
     let index = row.index(row.startIndex, offsetBy: x)
     return row[index]
+}
+
+func lookup(map: [String], pos: Location) -> Character? {
+    return lookup(map: map, y: pos.y, x: pos.x)
 }
 
 func visit(map: inout [String], location: Location, marker: Character) {
@@ -89,15 +113,7 @@ func walk(map: inout [String], start: Location, facing initialdir: Direction) ->
     var marker: Character = "X"
 
     while let newdir = move(map: map, current: position, facing: facing) {
-        if newdir.dy < 0 {
-            marker = "N"
-        } else if newdir.dy > 0 {
-            marker = "S"
-        } else if newdir.dx < 0 {
-            marker = "W"
-        } else {
-            marker = "E"
-        }
+        let marker = dirToLetter(newdir)
 
         let oldpos = position
         visit(map: &map, location: oldpos, marker: marker)
@@ -107,7 +123,7 @@ func walk(map: inout [String], start: Location, facing initialdir: Direction) ->
 
         if position == start {
             // completed the circle
-            visit(map: &map, location: position, marker: marker)
+            visit(map: &map, location: position, marker: dirToLetter(initialdir))
             print("completed the circle")
             return distance
         }
@@ -116,56 +132,93 @@ func walk(map: inout [String], start: Location, facing initialdir: Direction) ->
     return nil
 }
 
-func countInlcuded(map: [String]) -> (Int, Int) {
-    // first determine what the inside and the outside of the path is.
-    // when approaching from the left of the grid it is impossible to switch north/south directions
-
-    let directioncharacters: Set<Character> = ["N", "S", "E", "W", "\n"]
-
-    var northsouth: Character = "X"
-
-    for line in map {
-        let dir = line.first(where: { directioncharacters.contains($0) })!
-        if dir != "N" && dir != "S" {
-            continue
-        }
-
-        northsouth = dir
-        break
+func rotateLeft(_ dir: Direction) -> Direction {
+    switch dir {
+    case north: return west
+    case east: return north
+    case south: return east
+    case west: return south
+    case _: assert(false)
     }
-
-    var inside = 0
-    var outside = 0
-    
-    assert(northsouth == "N" || northsouth == "S")
-
-    let inmarker: Character = northsouth == "N" ? "N" : "S"
-    let outmarker: Character = northsouth == "N" ? "S" : "N"
-
-    for line in map {
-        var isIn = false
-        
-        for char in line {
-            if char == inmarker {
-                isIn = true
-            } else if char == outmarker {
-                isIn = false
-            }
-
-            if !directioncharacters.contains(char) {
-                if isIn {
-                    inside += 1
-                } else {
-                    outside += 1
-                }
-            }
-        }
-    }
-
-    return (inside, outside)
 }
 
-let path = "example.txt"
+func rotateRight(_ dir: Direction) -> Direction {
+    switch dir {
+    case north: return east
+    case east: return south
+    case south: return west
+    case west: return north
+    case _: assert(false)
+    }
+}
+
+func walk2(map: inout [String], start: Location) {
+    // first replace all non-path characters with a .
+    let dirchars: Set<Character> = ["N", "E", "S", "W"]
+    for (y, line) in map.enumerated() {
+        map[y] = String(line.map({ dirchars.contains($0) ? $0 : "." }))
+    }
+
+    // start at the start position and keep walking until we are at the start again
+    var pos = start
+    repeat {
+        let dir = letterToDir(lookup(map: map, pos: pos)!)!
+
+        // segment everything in "to the left" and "to the right" of the path
+        // test if the neighbouring characters are part of the path
+        let left = pos + rotateLeft(dir)
+        if let char = lookup(map: map, pos: left), char == "." {
+            visit(map: &map, location: left, marker: "L")
+        }
+
+        let right = pos + rotateRight(dir)
+        if let char = lookup(map: map, pos: right), char == "." {
+            visit(map: &map, location: right, marker: "R")
+        }
+        
+        let oldpos = pos
+        pos = pos + dir
+    } while pos != start
+}
+
+func floodFillAt(map: inout [String], location: Location, target: Character) {
+    visit(map: &map, location: location, marker: target)
+
+    for d in [north, east, south, west] {
+        let neighbour = location + d
+        if let c = lookup(map: map, pos: neighbour), c == "." {
+            visit(map: &map, location: neighbour, marker: target)
+            floodFillAt(map: &map, location: neighbour, target: target)
+        }
+    }
+}
+
+func floodFill(map: inout [String], target: Character) {
+    for (y, line) in map.enumerated() {
+        for (x, char) in line.enumerated() {
+
+            if char == target {
+                let loc: Location = (y: y, x: x)
+                floodFillAt(map: &map, location: loc, target: target)
+            }
+        }
+    }
+}
+
+func countChars(map: [String], target: Character) -> Int {
+    var count = 0
+    for line in map {
+        for c in line {
+            if c == target {
+                count += 1
+            }
+        }
+    }
+
+    return count
+}
+
+let path = "input.txt"
 let map = readFile(path)
 let start = findStart(map)!
 let char = lookup(map: map, y: start.y, x: start.x)
@@ -174,9 +227,16 @@ for d in [north, east, south, west] {
     var mutmap = map
     if let distance = walk(map: &mutmap, start: start, facing: d) {
         print("part 1: ", distance / 2)
-        print("part 2: ", countInlcuded(map: mutmap))
-        let directioncharacters: Set<Character> = ["N", "S", "E", "W", "\n"]
-        print(String(mutmap.joined(separator: "\n").map({ directioncharacters.contains($0) ? $0 : "."})))
+        print(String(mutmap.joined(separator: "\n")))
+        print("path")
+        walk2(map: &mutmap, start: start)
+        print(String(mutmap.joined(separator: "\n")))
+        print("marked")
+        floodFill(map: &mutmap, target: "L")
+        floodFill(map: &mutmap, target: "R")
+        print(String(mutmap.joined(separator: "\n")))
+        print("filled")
+        print(countChars(map: mutmap, target: "L"), countChars(map: mutmap, target: "R"))
         break
     }
 }

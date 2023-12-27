88f8f5d9020af05b139a09bfee5942147fa0e530
Max Verbeek <m4xv3rb33k@gmail.com>
2023-12-27 17:48:57 +0100

    feat: did some more day 23 but have to go again lol

--- 23/src/main.rs
+++ 23/src/main.rs
@@ -41,9 +41,11 @@ type Pos = (usize, usize);
 // transform the map into a graph, where each node represents a junction. each node will be an
 // index in a vector, and contains an array of neighbours, along with a number of steps to get
 // there.
-fn make_graph(tiles: &TileVec) -> Vec<(usize, usize)> {
+fn make_graph(tiles: &TileVec) -> Vec<Vec<(usize, usize)>> {
     let mut junction_to_node: HashMap<Pos, usize> = HashMap::new();
-    let mut graph = vec![];
+
+    let start: (usize, usize) = (1, 1);
+    let end = (tiles.len() - 2, tiles.last().unwrap().len() - 2);
 
     let height = tiles.len();
     let width = tiles[0].len();
@@ -51,12 +53,40 @@ fn make_graph(tiles: &TileVec) -> Vec<(usize, usize)> {
     // use 1 .. boundary - 1 because the boundary is surrounded with wall and not worth checking.
     // this way we dont need to check for out of bounds conditions
     for r in 1..(height - 1) {
-        for c in 1..(width - 1) {}
+        for c in 1..(width - 1) {
+            let pos = (r, c);
+
+            if get_neighbours(tiles, pos).count() > 2 || pos == start || pos == end {
+                // pos is a junction. first we create a new node specifically for this junction so
+                // that we can store it in the vec.
+                junction_to_node.insert(pos, junction_to_node.len());
+            }
+        }
+    }
+
+    let mut graph = vec![vec![]; junction_to_node.len()];
+
+    for r in 1..(height - 1) {
+        for c in 1..(width - 1) {
+            let pos = (r, c);
+
+            if junction_to_node.contains_key(&pos) {
+                // we are at a junction, walk in every direction until we encounter a neighbouring
+                // junction.
+            }
+        }
     }
 
     graph
 }
 
+fn get_neighbours<'a>(tiles: &'a TileVec, pos: Pos) -> impl Iterator<Item = Pos> + 'a {
+    let (r, c) = pos;
+
+    [(r - 1, c), (r, c + 1), (r + 1, c), (r, c - 1)]
+        .into_iter()
+        .filter(|&(r, c)| tiles[r][c] != '#')
+}
 fn walk(
     tiles: &TileVec,
     visited: &mut VV<bool>,
